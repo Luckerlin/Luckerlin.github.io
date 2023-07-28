d91d8f5d8b344058f75674b8fb942c243e2b2620
luckerlin <luckerlin@foxmail.com>
2023-07-28 10:24:50 +0800

    add an article c25746a5

---
+++ junewind/random.js
@@ -0,0 +1,1 @@
+var posts=["p/4a17b156/","p/dd4a5138/","p/7ea80d1a/","p/c25746a5/","p/f4adc632/","p/152e2c7f/","p/45ffd488/","p/encryption/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
